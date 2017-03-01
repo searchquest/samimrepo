6b14cc0d308f3c7cd916e359539a96e3efa0043f
samim akhtar <samim.akhtar@gmail.com>
2017-03-01 14:59:45 +0530

    Initialize repo

---
+++ socketdemo/public/js/app.js
@@ -0,0 +1,25 @@
+var socket=io();
+
+socket.on('connect', function(){
+	console.log('connected to socket.io server');
+});
+
+socket.on('message', function(message){
+	console.log(message.text);
+	
+	jQuery('.messages-from').append('<p>' + message.text + '</p>');
+});
+
+var $form = jQuery('#message-form');
+
+$form.on('submit', function(event) {
+	event.preventDefault();
+	
+	var $message = $form.find('input[name=message]');
+	
+	socket.emit('message', {
+		text: $message.val()
+	});
+	
+	$message.val('');
+})
